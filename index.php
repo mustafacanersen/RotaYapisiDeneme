8fe5392c59a06943bf085c0f58d1b787371e4569
Mustafa Can Ersen <mustafa.can.ersen@babursahturizm.com>
2023-07-20 12:24:46 +0300

    ikinci gönderim

--- index.php
+++ index.php
@@ -3,39 +3,70 @@
     $router = new AltoRouter();
     $router->setBasePath('/RotaYapisi');
     
-    $router->map( 'GET', '/', function(){
+    $router->map( 'GET', '/uye-girisi', function(){
         echo '<form action="" method="POST">
             <input name="id" placeholder="kullanıcı adı"><br>
             <input name="pass" placeholder="sifre"><br>
             <button type="submit">Giriş Yap</button>
         </form>
-        <a href="signUp.php" >
+        <a href="/uye-ol" >
         <button type="submit">Üye Ol</button>
         </a>';
     });
-    $id = $_POST['id'];
-    $pass = $_POST['pass'];
-
-    function girisKontrol(){
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "deneme";
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        $query ="SELECT * FROM uyebilgileri WHERE id = '$id' ";
-        $result = mysqli_query($conn,$query);
-        $row=mysqli_fetch_array($result);
-        if($id==$row["id"] && $pass==$row["pass"]){
-            echo "Hoşgeldin". $row['ad'];
-        }
-        else{
-            echo "kullanıcı adı veya şifre yanlış";
-        }
-        }
-        $router->map('POST', '/anasayfa', girisKontrol());
-
     
 
+        $router->map('POST', '/uye-girisi', function(){
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "deneme";
+            $id = $_POST['id'];
+            $pass = $_POST['pass'];
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            $query ="SELECT * FROM uyebilgileri WHERE id = '$id' ";
+            $result = mysqli_query($conn,$query);
+            $row=mysqli_fetch_array($result);
+            if($id==$row["id"] && $pass==$row["pass"]){
+                echo "Hoşgeldin". $row['ad'];
+            }
+            else{
+                echo "kullanıcı adı veya şifre yanlış";
+            }
+        });
+        $router->map('GET', '/uye-ol', function(){
+            include 'signUp.php';
+        });
+        $router->map('POST', '/uye-ol', function(){
+            $servername = "localhost";
+            $username = "root";
+            $pass = "";
+            $dbname = "deneme";
+            
+            $conn = mysqli_connect($servername, $username, $pass, $dbname);
+            
+            if (!$conn) {
+                die("Bağlantı hatası: " . mysqli_connect_error());
+            }
+            if(isset($_POST['id']) && isset($_POST['pass']) && isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['job']) && isset($_POST['birthday']) && isset($_POST['sex'])) {
+            $id = $_POST['id'];
+            $pass = $_POST['pass'];
+            $firstname = $_POST['firstname'];
+            $lastname = $_POST['lastname'];
+            $job = $_POST['job'];
+            $birthday = $_POST['birthday'];
+            $sex = $_POST['sex'];
+            
+            $sql = "INSERT INTO uyebilgileri (id, pass, firstname, lastname, job, birthday, sex)
+            VALUES ('$id', '$pass', '$firstname', '$lastname', '$job', '$birthday', '$sex')";
+            if (mysqli_query($conn, $sql)) {
+                header("Location: ");
+                exit();
+            }
+            else{
+                echo "Hata: " . $sql . "<br>" . mysqli_error($conn);
+            }
+            }
+            });
 
 
     $match = $router->match();
